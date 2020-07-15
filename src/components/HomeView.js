c89156e47f526d91811c71d661a4ea410d23a9c3
grantkoji <grantkoji@gmail.com>
2020-07-15 12:59:32 -0400

    grant y - core deliverables only

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -8,6 +8,7 @@ class HomeView extends React.Component {
     state = {
         plants: [],
         showCreateForm: false,
+        search: ''
     }
 
     /** DO NOT WORRY ABOUT THIS COMPONENTDIDMOUNT METHOD
@@ -21,27 +22,34 @@ class HomeView extends React.Component {
 
     toggleCreateForm = () => this.setState({ showCreateForm: !this.state.showCreateForm })
 
-
+    
     /**
      * TODO: ONLY FOR ADVANCED DELIVERABLES
      * Once you post a new plant, you'll need to update the plants on state here.
      * Define a method that can add a new plant into the plants array.
      */
 
+    
+    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
     render(){
+        let filteredPlants = this.state.plants.filter(plant => 
+            plant["Common_Name"].toLowerCase().includes(this.state.search.toLowerCase()))
+      
+
         const { plants, showCreateForm } = this.state
         // TODO: In order to search, what state, methods and element attributes are needed? 
         // In order to render the correct plants, what calculations do you need to do and what props do you need to change below?
-
+        // "Common_Name"
         return (
             <div>
                 <button onClick={this.toggleCreateForm}>{showCreateForm ? "Hide Form" : "Submit Plant"}</button>
                 { showCreateForm && <CreatePlantForm />}
                 <hr />
                 <div>
-                    <input placeholder="Search for Plants"/>
+                    <input placeholder="Search for Plants" name="search" onChange={this.handleChange} value={this.state.search}/>
                 </div>
-                <MatchContainer plants={plants}/>
+                <MatchContainer plants={filteredPlants}/>
             </div>
         )
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,43 @@
 import React from 'react';
+import HomeView from './HomeView'
 
 class Login extends React.Component {
     state = {
+        name: '',
+        username: '',
+        password: '',
+        confirmPassword: ''
         // TODO: What needs to be represented in state for a fully controlled form?
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
     // this.props.changeView('home')
 
+    loginSubmitHandler = event => {
+        event.preventDefault()
+        if (this.state.password === this.state.confirmPassword) {
+        this.props.componentDidMount()
+        this.props.changeView('home')
+        } else {
+            alert("Password and Confirm Password must match.")
+        }
+    }
+
     render(){
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
-            <form className="vertical-flex">
+            <form className="vertical-flex" onSubmit={event => this.loginSubmitHandler(event)}>
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
+                <input placeholder="Name" name="name" value={this.state.name} onChange={event => this.handleChange(event)}/>
+                <input placeholder="Username" name="username" value={this.state.username} onChange={event => this.handleChange(event)}/>
+                <input placeholder="Password" name="password" value={this.state.password} onChange={event => this.handleChange(event)}/>
+                <input placeholder="Confirm Password" name="confirmPassword" value={this.state.confirmPassword} onChange={event => this.handleChange(event)}/>
                 <button type="submit">Submit</button>
             </form>
         )
